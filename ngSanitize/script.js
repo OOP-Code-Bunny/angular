c5993095bbc413588d625a92ae5f5080375df3e5
OOP-Code-Bunny <947158704@qq.com>
2014-11-20 10:36:09 +0800

    ng

--- ngSanitize/script.js
+++ ngSanitize/script.js
@@ -6,6 +6,16 @@ app.controller('ctrl',function($scope,$sce){
     $scope.trustHtml = $sce.trustAsHtml($scope.myHtml)
 });
 
+app.controller('ctrlLinky',function($scope,$sce,$filter){
+    $scope.myHtml = '<p>'+
+    '下面这些都应该是链接:\n'+
+    'http://angularjs.org/,\n'+
+    'mailto:us@somewhere.org,\n'+
+    'another@somewhere.org,\n'+
+    'and one more: ftp://127.0.0.1/.\n'+
+    '</p>';
+    console.log($filter('linky')($scope.myHtml,'_blank'))
+});
 /*
 1.ngSanitize添加了$sanitize服务,但这个服务是不出现在代码里的,它的出现的作用就是使得html里面可以使用ng-bind-html这个指令,能够把ng-bind-html绑定的内容进行安全编译
   安全编译的方法就是移除html上的所有属性,是所有属性,包括事件绑定,样式,等一切.仅仅保留html标签和内容.
